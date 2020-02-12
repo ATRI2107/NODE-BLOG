3646ff143ad3017453927bf3503e1d7885a245a0
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-13 02:06:29 +0530

    Posts routes configured and added the POST route

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var mongo = require('mongodb');
+var db= require('monk')('localhost/nodeblog');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/add', function(req, res, next) {
+  var categories=db.get('categories');
+  categories.find({},{},(err,categories)=>{
+    res.render('addpost',{
+      'title': "Add Post",
+      'categories': categories
+    });
+  });
 });
+router.post("/add",upload.single('mainimage'),function(req,res,next){
+  var title=req.body.title,
+      category=req.body.category,
+      body=req.body.body,
+      author=req.body.author,
+      date=new Date();
+  if(req.file)
+  {
+    var mainimage=req.file.filename;
+  }
+  else
+  {
+    var mainimage='noimage.jpg'
+  }
+
+  //Form Validation
+  req.checkBody('title','Title field is required').notEmpty();
+  req.checkBody('body',"Body is required").notEmpty();
+
+  //check errors
+  var errors=rqe.validationErrors();
+  if(errors)
+  {
+    res.render('addpost',{
+      "errors": errors
+    });
+  }
+  else{
+    var posts=db.get('posts')
+    posts.insert({
+      "title": title,
+      "body": body,
+      "category": category,
+      "date": date,
+      "author": author,
+      "mainimage": mainimage
+    },(err,post)=>{
+      if(err)
+      {
+        res.send(err);
+      }
+      else
+      {
+        req.flash('success','Post Addedd');
+        res.location('/');
+        res.redirect('/');
+      }
+    })
+  }
+
+})
 
 module.exports = router;
