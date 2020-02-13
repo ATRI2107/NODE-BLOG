bbae4dffba562653347e769a627532be40d99a7c
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-14 03:07:48 +0530

    Route set up for individual posts viewing

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,15 @@ var upload = multer({ dest: './public/images' });
 var mongo = require('mongodb');
 var db= require('monk')('localhost/nodeblog');
 
+router.get('/show/:id',(req,res,next)=>{
+  var posts=db.get('posts');
+  posts.findById(req.params.id,(err,post)=>{
+    res.render('show',{
+      'post': post
+    });
+  });
+});
+
 router.get('/add', function(req, res, next) {
   var categories=db.get('categories');
   categories.find({},{},(err,categories)=>{
