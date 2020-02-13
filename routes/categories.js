24e991e494caef185d7e1159be75b1c6101d9d3f
ATRI2107 <atrichaturvedi@gmail.com>
2020-02-13 19:50:48 +0530

    Categories post route added and configured

--- routes/categories.js
+++ routes/categories.js
@@ -8,5 +8,38 @@ router.get('/add', function(req, res, next) {
       'title': "Add Category"
   });
 });
+router.post("/add",function(req,res,next){
+    var name=req.body.name;
+    
+    //Form Validation
+    req.checkBody('name','Name field is required').notEmpty();
+
+    //check errors
+    var errors=req.validationErrors();
+    if(errors)
+    {
+      res.render('addpost',{
+        "errors": errors
+      });
+    }
+    else{
+      var categories=db.get('categories')
+      categories.insert({
+        "name": name
+      },(err,category)=>{
+        if(err)
+        {
+          res.send(err);
+        }
+        else
+        {
+          req.flash('success','Category Addedd');
+          res.location('/');
+          res.redirect('/');
+        }
+      })
+    }
+  
+  });
 
 module.exports = router;
